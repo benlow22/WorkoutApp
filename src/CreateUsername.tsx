62d7e40fc2dae67d8cbe7dedaaa8d522ebb13928
benlow22 <ben2.2low@gmail.com>
2023-04-04 16:11:44 -0700

    change username via update bar

--- src/CreateUsername.tsx
+++ src/CreateUsername.tsx
@@ -6,9 +6,15 @@ import { Input, Button, Space } from "antd";
 export const CreateUsername: React.FC<{}> = () => {
 	const [newUsername, setNewUsername] = useState<string>("");
 	const [user, setUser] = useState<any | null>(null);
-	
-	const handleSubmit = async (e:any) => {
+
+	const handleSubmit = async (e: any) => {
 		e.preventDefault();
+		console.log("new Username to set", newUsername);
+
+		const {
+			data: { user },
+		} = await supabase.auth.getUser();
+		console.log("user info", user);
 		const { error } = await supabase
 			.from("profiles")
 			.update({ username: newUsername })
@@ -23,6 +29,7 @@ export const CreateUsername: React.FC<{}> = () => {
 					defaultValue="Username"
 					onChange={(e) => setNewUsername(e.target.value)}
 					value={newUsername}
+					placeholder="New Username"
 				/>
 				<Button type="primary" onClick={handleSubmit}>
 					Submit
