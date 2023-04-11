c33bba8c6226dca8094287a1418553b26c724600
benlow22 <ben2.2low@gmail.com>
2023-04-11 11:12:25 -0700

    updata db file

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IWorkout } from './data';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): IWorkout[] {
     return this.appService.getHello();
   }
 
