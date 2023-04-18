c4cb29df124e1791d79be1aa8d9540d64c9806b3
benlow22 <ben2.2low@gmail.com>
2023-04-18 14:54:12 -0700

    move get workout name to api file

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -12,18 +12,22 @@ export const getWorkouts = async () => {
 	return data;
 };
 
-// getWorkoutDay = gets a workoutName, returns all exercises in Name.
-
+// getWorkoutDay = 
 export const getWorkoutDay = async (workoutName: string = "") => {
 	const { data, error } = await supabase
 		.from("workouts")
-		.select("name,id")
+		.select("name, url")
 		.eq("url", workoutName);
+	console.log("get that data:", data);
 	if (error) {
 		console.error(error);
 		return;
+	} else { 
+		if (data.length > 0) {
+			console.log('DATATATAT:', data[0])
+			return data[0];
+		}
 	}
-	return data;
 };
 
 /* postNewWorkout = checks if workout already exists by url ( since that is what will be searched and entered  backAndBi === back and bi), then adds to Workout table (including:  
@@ -47,30 +51,31 @@ export const postNewWorkout = async (
 	console.log("empty[] if url is free to take:", existingWorkoutUrl);
 	if (error) {
 		console.log("there was an error with finding if workout exists", error);
-		throw "error finding workout in DB, two may have been found"
+		throw "error finding workout in DB, two may have been found";
 	}
 	if (existingWorkoutUrl.length > 0) {
 		alert(
 			"Sorry Workout Name is already in use or too similar to another Workout you have. Please select a new name"
 		);
-		throw "Workout name already in DB"
+		throw "Workout name already in DB";
 	} else {
 		console.log("workout was not found in DB, time to add it in");
 		const newWorkoutObj = {
 			// check if i need uuid or supabase will autofill
 			name: workoutName,
 			url: url,
-			user_id: user_id
+			user_id: user_id,
 		};
 		// insert and return
 		const { data, error } = await supabase
 			.from("workouts")
-			.insert([newWorkoutObj]).select();
+			.insert([newWorkoutObj])
+			.select();
 		if (error) {
 			console.log("error", error);
 			throw error;
 		} else {
-			console.log('Successfully added to DB, go CHECK', data[0].url)
+			console.log("Successfully added to DB, go CHECK", data[0].url);
 			return data[0].url;
 		}
 	}
--- frontend/src/pages/EditWorkoutPage.tsx
+++ frontend/src/pages/EditWorkoutPage.tsx
@@ -10,36 +10,37 @@ export const EditWorkoutPage = () => {
 	const [data, setData] = useState<IWorkout | null>(null);
 	// data consist of name and id
 
+	// useEffect(() => {
+	// 	// get's workoutDay from db and di
+	// 	async function getWorkoutDay(workoutName: string = "") {
+	// 		const { data, error } = await supabase
+	// 			.from("workouts")
+	// 			.select("name, url")
+	// 			.eq("url", workoutName);
+	// 		if (error) {
+	// 			console.error(error);
+	// 			return;
+	// 		}
+	// 		console.log("DATA id: ", data);
+	// 		setData(data[0]);
+	// 	}
+	// 	getWorkoutDay(workoutName);
+	// }, [workoutName]);
+
+	// gets workout day from url, return name and id, to use to get exercises
 	useEffect(() => {
-		// get's workoutDay from db and di
-		async function getWorkoutDay(workoutName: string = "") {
-			const { data, error } = await supabase
-				.from("workouts")
-				.select("name, url")
-				.eq("url", workoutName);
-			if (error) {
-				console.error(error);
-				return;
+		async function getWorkout() {
+			const data = await getWorkoutDay(workoutName);
+			if (data) {
+				setData(data);
 			}
-			console.log("DATA id: ", data);
-			setData(data[0]);
 		}
-		getWorkoutDay(workoutName);
+		getWorkout();
 	}, [workoutName]);
 
 	useEffect(() => {
 		console.log("data update 1");
 	}, [data]);
-
-	// useEffect(() => {
-	// 	// get's workoutDay from db and di
-	// 	async function getWorkout() {
-	// 		const [name, id] = await getWorkoutDay(workoutName);
-	// 		setData(name);
-	// 		console.log("workout id: ", id);
-	// 	}
-	// 	getWorkout();
-	// }, [workoutName]);
 
 	if (!data) {
 		// loading page, while waiting for data to be fetched
