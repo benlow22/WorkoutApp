a6cefecee1bf2f6fe67b1417d74f005df7c40c5a
benlow22 <ben2.2low@gmail.com>
2023-04-19 02:30:53 -0700

    remove context provider to own file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 import "./App.css";
 import { Header } from "./components/Header";
@@ -9,40 +9,28 @@ import { WorkoutsPage } from "./pages/WorkoutsPage";
 import { IWorkout, workouts } from "./data";
 import { CreateUsername } from "./components/CreateUsername";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { NewWorkoutPage } from "./pages/NewWorkoutPage";
 import { EditWorkoutPage } from "./pages/EditWorkoutPage";
-import { hydrate, QueryClient, QueryClientProvider } from "react-query";
-type IAuthContext = {
-	userId: string;
-	username: string;
-	isLoggedIn: boolean;
-	workouts: IWorkout[];
-	setIsLoggedIn: (loggedIn: boolean) => void;
-	setUsername: (newName: string) => void;
-	setWorkouts: (usersWorkouts: IWorkout[]) => void;
-};
-
-export const AuthContext = React.createContext<IAuthContext>({
-	userId: "",
-	username: "",
-	isLoggedIn: false,
-	workouts: [],
-	setIsLoggedIn: () => {},
-	setUsername: () => {},
-	setWorkouts: () => {},
-});
+import AuthProvider, { AuthContext } from "./contexts/AuthProvider";
 
 export default function App() {
-	// const [QueryClient] = useState(()=> new QueryClient());
-	const [userId, setUserId] = useState<string>("");
-	const [username, setUsername] = useState<string>("");
-	const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+	const {
+		userId,
+		username,
+		isLoggedIn,
+		workouts,
+		isLoading,
+		setIsLoggedIn,
+		setUsername,
+		setWorkouts,
+		setUserId,
+		setIsLoading,
+	} = useContext(AuthContext);
 	const [session, setSession] = useState<any | null>(null);
-	const [isLoading, setIsLoading] = useState<boolean>(true);
-	const [workouts, setWorkouts] = useState<IWorkout[]>([]);
 
 	useEffect(() => {
+		setIsLoading(true)
 		supabase.auth.getSession().then(({ data: { session } }) => {
 			setSession(session);
 		});
@@ -51,8 +39,10 @@ export default function App() {
 		} = supabase.auth.onAuthStateChange((_event, session) => {
 			setSession(session);
 		});
+		setIsLoading(false)
 		return () => subscription.unsubscribe();
-	}, []);
+	}, [isLoading]);
+
 
 	useEffect(() => {
 		if (session) {
@@ -74,7 +64,10 @@ export default function App() {
 				data: { user },
 				error,
 			} = await supabase.auth.getUser();
-			user && setUserId(user.id);
+
+			if (user) {
+				setUserId(user.id);
+			}
 		} catch (error) {
 			console.error("Error while getting User", error);
 		}
@@ -93,105 +86,90 @@ export default function App() {
 				username = data.username;
 			}
 			username && setUsername(username);
+			if (error) {
+				console.log("ERROR getting userName: ", error);
+			}
 		} catch (error) {
 			console.error("Error while getting Username", error);
 		}
 	};
 
-	// if (!isLoading) {
-	// 	return <p>Loading</p>
-	// } else
-
-	// const getUserId = async () => {
-	// 	try {
-	// 		const {
-	// 			data: { user },
-	// 			error,
-	// 		} = await supabase.auth.getUser();
-	// 		user && setUserId(user.id);
-	// 	} catch (error) {
-	// 		console.error("Error while getting User", error);
-	// 	}
-	// };
-	// getUserId();
-	// console.log("after the big GET", userid);
-	// const getUsername = async () => {
-	// 	// try {
-
-	// 	// }
-	// }
-	// setIsLoggedIn(true);
-
-	// if IsLoading is TRUE then set to LOADING screen
-
-	// if isLoggedIn = false = Auth Path
-	// if (isLoggedIn) {
-	// 	return (
-
-	// 	)
-	// }
-	//
-	if (session && !isLoading && isLoggedIn) {
-		return (
-			<AuthContext.Provider
-				value={{
-					userId,
-					username,
-					isLoggedIn,
-					workouts,
-					setIsLoggedIn,
-					setUsername,
-					setWorkouts,
-				}}
-			>
+	return (
+		<>
+			{!isLoggedIn && !isLoading ? (
 				<div className="App">
 					<Header />
-					<div className="main">
-						{/* {!username && <CreateUsername />} */}
-						<Switch>
-							{" "}
-							{/* when invalid workout/name = redirect to homepage */}
-							<Route path="/workouts/:workoutName">
-								<EditWorkoutPage />
-							</Route>
-							<Route path="/newWorkout">
-								<NewWorkoutPage />
-							</Route>
-							<Route path="/">
-								<WorkoutsPage />
-							</Route>
-						</Switch>
+					<div className="auth-page">
+						<p>Please Login Below</p>
+						<Auth
+							supabaseClient={supabase}
+							appearance={{
+								theme: ThemeSupa,
+								variables: {
+									default: {
+										colors: {
+											brand: "red",
+											brandAccent: "darkred",
+										},
+									},
+								},
+							}}
+						/>
 					</div>
 				</div>
-			</AuthContext.Provider>
-		);
-	} else if (!session && !isLoading && !isLoggedIn) {
-		return (
-			<div>
-				<Header />
-				<p>Please Login Below</p>
-				<Auth
-					supabaseClient={supabase}
-					appearance={{
-						theme: ThemeSupa,
-						variables: {
-							default: {
-								colors: {
-									brand: "red",
-									brandAccent: "darkred",
-								},
-							},
-						},
-					}}
-				/>
-			</div>
-		);
-	} else {
-		return (
-			<div>
-				<Header />
-				<h2>Loading...</h2>
-			</div>
-		);
-	}
+			) : (
+				<div className="main">
+					{/* {!username && <CreateUsername />} */}
+					{isLoggedIn && (
+						<>
+							<Header />
+							<Switch>
+								{" "}
+								{/* when invalid workout/name = redirect to homepage */}
+								<Route path="/workouts/:workoutName">
+									<EditWorkoutPage />
+								</Route>
+								<Route path="/newWorkout">
+									<NewWorkoutPage />
+								</Route>
+								<Route path="/">
+									<WorkoutsPage />
+								</Route>
+							</Switch>
+						</>
+					)}
+				</div>
+			)}
+		</>
+	);
 }
+// 	} else if (!session && !isLoading && !isLoggedIn) {
+// 		return (
+// 			<div>
+// 				<Header />
+// 				<p>Please Login Below</p>
+// 				<Auth
+// 					supabaseClient={supabase}
+// 					appearance={{
+// 						theme: ThemeSupa,
+// 						variables: {
+// 							default: {
+// 								colors: {
+// 									brand: "red",
+// 									brandAccent: "darkred",
+// 								},
+// 							},
+// 						},
+// 					}}
+// 				/>
+// 			</div>
+// 		);
+// 	} else {
+// 		return (
+// 			<div>
+// 				<Header />
+// 				<h2>Loading...</h2>
+// 			</div>
+// 		);
+// 	}
+// }
