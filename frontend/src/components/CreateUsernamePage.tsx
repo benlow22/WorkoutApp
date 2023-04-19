a9114f11c7663fc5859a7ed2025db873dfb076d1
benlow22 <ben2.2low@gmail.com>
2023-04-19 15:56:12 -0700

    create minLength for new username =  >5char

--- frontend/src/components/CreateUsernamePage.tsx
+++ frontend/src/components/CreateUsernamePage.tsx
@@ -3,7 +3,7 @@ import { supabase } from "../supabaseClient";
 import { UserOutlined } from "@ant-design/icons";
 import { Input, Button, Space } from "antd";
 import { AuthContext } from "../contexts/AuthProvider";
-import { useHistory } from "react-router-dom"; 
+import { useHistory } from "react-router-dom";
 
 export const CreateUsernamePage: React.FC<{}> = () => {
 	const [newUsername, setNewUsername] = useState<string>("");
@@ -12,31 +12,33 @@ export const CreateUsernamePage: React.FC<{}> = () => {
 	const history = useHistory();
 	const handleSubmit = async (e: any) => {
 		e.preventDefault();
-		console.log("new Username to set", newUsername);
-
-		console.log("user info", user);
-		try {
-			const { error } = await supabase
-				.from("profiles")
-				.update({ username: newUsername })
-				.eq("id", userid);
-			setUsername(newUsername);
-			setNewUsername("");
-			history.push('/workouts');
-		} catch (err) {
-			console.log("error updating username", err);
+		if (newUsername.length > 4) {
+			console.log("new Username to set", newUsername);
+			console.log("user info", user);
+			try {
+				const { error } = await supabase
+					.from("profiles")
+					.update({ username: newUsername })
+					.eq("id", userid);
+				setUsername(newUsername);
+				setNewUsername("");
+				history.push("/workouts");
+			} catch (err) {
+				console.log("error updating username", err);
+			}
 		}
 	};
 
 	return (
 		<div>
-			<h2>{username ? 'Change ' : 'Create ' } Username</h2> 
+			<h2>{username ? "Change " : "Create "} Username</h2>
 			<Space.Compact>
 				<Input
 					defaultValue="Username"
 					onChange={(e) => setNewUsername(e.target.value)}
 					value={newUsername}
 					placeholder="New Username"
+					min={5}
 				/>
 				<Button type="primary" onClick={handleSubmit}>
 					Submit
