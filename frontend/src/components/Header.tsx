bc17a4842525c958c15ff40f5aa561e4d8e181f2
benlow22 <ben2.2low@gmail.com>
2023-04-19 15:25:10 -0700

    add fetch to header for username to delay

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,24 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import LogoutButton from "./LogOutButton";
 import { AuthContext } from "../contexts/AuthProvider";
 import { Link, BrowserRouter as Router, Route } from "react-router-dom";
+import { supabase } from "../supabaseClient";
 
 export const Header: React.FC<{}> = () => {
-	const { username, isLoggedIn } = useContext(AuthContext);
+	const { username, isLoggedIn, setUsername, userId } =
+		useContext(AuthContext);
+	const [isReady, setIsReady] = useState<boolean>(false);
 
 	useEffect(() => {
-		if (username) {
-			console.log(`${username} is logged in`);
-		}
+		const getUsername = async () => {
+			const { data, error } = await supabase.from('profiles').select('username').eq('id', userId);
+			console.log('ASDF ASDF ', data)
+			if (data && data.length > 0) {
+				setUsername(data[0].username)
+				setIsReady(true);
+			}
+		};
+		getUsername();
 	}, [isLoggedIn]);
 
 	return (
@@ -18,7 +27,7 @@ export const Header: React.FC<{}> = () => {
 				<h1>Workout Buddy</h1>
 			</Link>
 			<div className="account">
-				{isLoggedIn && (
+				{(isLoggedIn && isReady) && (
 					<>
 						<Link to="/createUsername">
 							{!username ? "Create Username" : username}
