4393b607a06912a42ecad4af43f145ab17de4e2c
benlow22 <ben2.2low@gmail.com>
2023-04-25 12:12:43 -0700

    remove context from AuthPage

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -3,26 +3,14 @@ import { supabase } from "../supabaseClient";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../contexts/AuthProvider";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 export const AuthPage = () => {
-	// if (isLoggedIn) {
-	// 	return <Navigate to="/workouts" />;
-	// }
-    const navigate = useNavigate();
-
-	const {
-		userId,
-		username,
-		isLoggedIn,
-		setIsLoggedIn,
-		setUsername,
-		setUserId,
-	} = useContext(AuthContext);
 	const [session, setSession] = useState<any | null>(null);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 
-    // when going to AuthPage, get session, set if logged in
+
+	// when going to AuthPage, get session, set if logged in
 	useEffect(() => {
 		supabase.auth.getSession().then(({ data: { session } }) => {
 			setSession(session);
@@ -36,95 +24,27 @@ export const AuthPage = () => {
 		return () => subscription.unsubscribe();
 	}, []);
 
-
-    // if there is a session, get userId (sets it to provider), turn logged in to TRUE
-	useEffect(() => {
-		if (session) {
-			getUserId();
-			setIsLoggedIn(true);
-		}
-	}, [session]);
-
-    // once user ID is set/changed, get username 
-	useEffect(() => {
-		if (userId) {
-			getUsername();
-		}
-	}, [userId]);
-
-    //once username is set or changed, check if id does not nexists, then set logged in to false 
-    // logs user out if userID is gone, unnessary ??? 
-	useEffect(() => {
-		if (!userId) {
-			setIsLoggedIn(false);
-		}
-	}, [username]);
-
-    // once logged in and username is gotten, redirect to workouts 
-    
-	useEffect(() => {
-		if (isLoggedIn) {
-			navigate('/workouts')
-		}
-	}, [username]);
-
-
-
-
-	const getUserId = async () => {
-		try {
-			const {
-				data: { user },
-				error,
-			} = await supabase.auth.getUser();
-
-			if (user) {
-				setUserId(user.id);
-			}
-		} catch (error) {
-			console.error("Error getting User", error);
-		}
-	};
-
-	const getUsername = async () => {
-		try {
-			const { data, error } = await supabase
-				.from("profiles")
-				.select("username")
-				.eq("id", userId)
-				.limit(1)
-				.single();
-			let username;
-			if (data) {
-				username = data.username;
-			}
-			username && setUsername(username);
-			if (error) {
-				console.log("ERROR getting userName: ", error);
-			}
-			setIsLoading(false);
-		} catch (error) {
-			console.error("Error while getting Username", error);
-		}
-	};
-
-	return (
-		<div className="auth-page">
-			<p>Please Login Below</p>
-			<Auth
-				supabaseClient={supabase}
-				appearance={{
-					theme: ThemeSupa,
-					variables: {
-						default: {
-							colors: {
-								brand: "red",
-								brandAccent: "darkred",
+	if (!session) {
+		return (
+			<div className="auth-page">
+				<p>Please Login Below</p>
+				<Auth
+					supabaseClient={supabase}
+					appearance={{
+						theme: ThemeSupa,
+						variables: {
+							default: {
+								colors: {
+									brand: "red",
+									brandAccent: "darkred",
+								},
 							},
 						},
-					},
-				}}
-			/>
-		</div>
-	);
+					}}
+				/>
+			</div>
+		);
+	} else {
+		return <Navigate to="/workouts" />;
+	}
 };
