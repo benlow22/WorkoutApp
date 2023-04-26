9f6cec0870d001f6a23d6ad3c91371510643d728
benlow22 <ben2.2low@gmail.com>
2023-04-26 15:12:04 -0700

    change  to render if not auth (connect to context)

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -6,26 +6,12 @@ import { AuthContext } from "../contexts/AuthProvider";
 import { Navigate, useNavigate } from "react-router-dom";
 
 export const AuthPage = () => {
-	const [session, setSession] = useState<any | null>(null);
-	const [isLoading, setIsLoading] = useState<boolean>(true);
-
+	const { auth } = useContext(AuthContext);
 
 	// when going to AuthPage, get session, set if logged in
-	useEffect(() => {
-		supabase.auth.getSession().then(({ data: { session } }) => {
-			console.log('session data', session)
-			setSession(session);
-		});
-		const {
-			data: { subscription },
-		} = supabase.auth.onAuthStateChange((_event, session) => {
-			setSession(session);
-		});
-		setIsLoading(false);
-		return () => subscription.unsubscribe();
-	}, []);
 
-	if (!session) {
+
+	if (!auth) {
 		return (
 			<div className="auth-page">
 				<p>Please Login Below</p>
