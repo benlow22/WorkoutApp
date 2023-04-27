9f669976d53fb18a42cf5096658bb6389a4061a0
benlow22 <ben2.2low@gmail.com>
2023-04-26 20:02:26 -0700

    get Exercise names from DB

--- frontend/src/pages/ExercisesPage.tsx
+++ frontend/src/pages/ExercisesPage.tsx
@@ -11,39 +11,51 @@ const { Search } = Input;
 export const ExercisesPage: React.FC<{}> = () => {
 	const { workouts, setWorkouts, userId } = useContext(AuthContext);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [allExercises, setAllExercises] = useState<any>([]); //change to appropriate type
 
 	console.log("Exercises page");
-	// useEffect(() => {
-	// 	console.log("workouts fetch");
 
-	// 	async function fetchWorkouts() {
-	// 		const { data, error } = await supabase
-	// 			.from("workouts")
-	// 			.select("name,url,id");
-	// 		if (error) {
-	// 			console.error(error);
-	// 			setIsLoading(false);
-	// 			return;
-	// 		}
-	// 		setWorkouts(data);
-	// 		setIsLoading(false);
-	// 		return data;
-	// 	}
-	// 	fetchWorkouts();
-	// }, []);
+	useEffect(() => {
+		console.log("exercises fetch", allExercises);
+
+		async function fetchExercises() {
+			const { data, error } = await supabase
+				.from("Exercises")
+				.select("name");
+			if (error) {
+				console.error("error fetching exercises", error);
+				setIsLoading(false);
+				return;
+			}
+			if (data.length > 0) {
+				console.log("fetching exercises data: ", data);
+				setAllExercises(data);
+			}
+			setIsLoading(false);
+			return data;
+		}
+		fetchExercises();
+		console.log("exercises fetch2", allExercises);
+	}, []);
 
 	const onSearch = (value: string) => console.log(value);
-
+	useEffect(() => {
+		if (allExercises) {
+			console.log("all exercises updated", allExercises);
+		}
+	}, [allExercises]);
 	return (
 		<div className="exercise-page">
-			<h2>Exercises</h2>
+			<h2>Exercises </h2>
 			<Search
 				placeholder="Exercise Search"
 				onSearch={onSearch}
 				enterButton
 				className="exercise-search"
 			/>
-            
+			{allExercises.map((exercise: any) => (
+				<p>{exercise.name}</p>
+			))}
 		</div>
 	);
 };
