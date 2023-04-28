d0d950576a424f8b5cbc7552b0605475f9bd47f3
benlow22 <ben2.2low@gmail.com>
2023-04-28 13:14:41 -0700

    change font of dropdown search bar

--- frontend/src/pages/ExercisesPage.tsx
+++ frontend/src/pages/ExercisesPage.tsx
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import WorkoutButton from "../components/WorkoutButton";
-import { Button } from "antd";
+import { Button, theme, ConfigProvider } from "antd";
 import { Link, NavLink, Route } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthProvider";
 import { supabase } from "../supabaseClient";
 import { Input, Space } from "antd";
+import { AutoComplete } from "antd";
 
 const { Search } = Input;
 
@@ -12,9 +13,14 @@ export const ExercisesPage: React.FC<{}> = () => {
 	const { workouts, setWorkouts, userId } = useContext(AuthContext);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 	const [allExercises, setAllExercises] = useState<any>([]); //change to appropriate type
-
+	const { useToken } = theme;
 	console.log("Exercises page");
-
+    const options = [
+        { value: "Burns Bay Road" },
+        { value: "Downing Street" },
+        { value: "Wall Street" },
+    ];
+    
 	useEffect(() => {
 		console.log("exercises fetch", allExercises);
 
@@ -47,15 +53,47 @@ export const ExercisesPage: React.FC<{}> = () => {
 	return (
 		<div className="exercise-page">
 			<h2>Exercises </h2>
-			<Search
-				placeholder="Exercise Search"
-				onSearch={onSearch}
-				enterButton
-				className="exercise-search"
-			/>
+			<div className="search-container">
+				<ConfigProvider
+					theme={{
+						token: {
+							colorTextBase: "rgba(0,0,0,0)",
+						},
+					}}
+				>
+					<AutoComplete
+						defaultOpen={true}
+						open={true}
+						allowClear
+						onDropdownVisibleChange={() => {}}
+						style={{
+							width: 200,
+						}}
+						popupClassName="thingys"
+						options={options}
+						placeholder="Search Exercises`"
+						filterOption={(inputValue, option) =>
+							option!.value
+								.toUpperCase()
+								.indexOf(inputValue.toUpperCase()) !== -1
+						}
+					/>
+
+					<Button type="primary">Submit</Button>
+					<Search
+						placeholder="Exercise Search"
+						onSearch={onSearch}
+						enterButton
+						className="exercise-search"
+					/>
+					<div className="suggestions"></div>
+				</ConfigProvider>
+			</div>
+
 			{allExercises.map((exercise: any) => (
 				<p>{exercise.name}</p>
 			))}
 		</div>
 	);
 };
+
