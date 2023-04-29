af5c515e35b7da128a02f007c9fbe2f3e9a0e42e
benlow22 <ben2.2low@gmail.com>
2023-04-28 17:21:15 -0700

    get names of exercises, with typing

--- frontend/src/pages/ExercisesPage.tsx
+++ frontend/src/pages/ExercisesPage.tsx
@@ -12,23 +12,35 @@ const { Search } = Input;
 export const ExercisesPage: React.FC<{}> = () => {
 	const { workouts, setWorkouts, userId } = useContext(AuthContext);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
-	const [allExercises, setAllExercises] = useState<any>([]); //change to appropriate type
+	const [allExercises, setAllExercises] = useState<{ value: string }[]>([]); //change to appropriate type
 	const [searchExercise, setSearchExercise] = useState<string>("");
 
 	console.log("Exercises page");
+
 	const options = [
 		{ value: "Burns Bay Road" },
 		{ value: "Downing Street" },
 		{ value: "Wall Street" },
+		{ value: "Burns Bay1 Road" },
+		{ value: "Downing1 Street" },
+		{ value: "Wall Str1eet" },
+		{ value: "Burns Bay1a Road" },
+		{ value: "Downing St1reet" },
+		{ value: "Wall Street1" },
+		{ value: "Burns Bay Ro1ad" },
+		{ value: "Downing Stree1t" },
+		{ value: "Wall Street2" },
+		{ value: "Burns Bay Roa2d" },
+		{ value: "Downing Stre2et" },
+		{ value: "Wall Stree2t" },
 	];
 
 	useEffect(() => {
 		console.log("exercises fetch", allExercises);
-
 		async function fetchExercises() {
 			const { data, error } = await supabase
 				.from("Exercises")
-				.select("name");
+				.select("value: name");
 			if (error) {
 				console.error("error fetching exercises", error);
 				setIsLoading(false);
@@ -51,6 +63,7 @@ export const ExercisesPage: React.FC<{}> = () => {
 			console.log("all exercises updated", allExercises);
 		}
 	}, [allExercises]);
+
 	return (
 		<div className="exercise-page">
 			<h2>Exercises </h2>
@@ -60,8 +73,12 @@ export const ExercisesPage: React.FC<{}> = () => {
 					style={{
 						width: 260,
 					}}
+					notFoundContent={`new exercise: ${searchExercise}`}
+					defaultOpen={false}
+					open
+					defaultActiveFirstOption
 					onChange={(value) => setSearchExercise(value)}
-					options={options}
+					options={allExercises}
 					children={
 						<Search
 							placeholder="Exercise Search"
@@ -85,3 +102,6 @@ export const ExercisesPage: React.FC<{}> = () => {
 		</div>
 	);
 };
+// ant-select-dropdown css-dev-only-do-not-override-yp8pcc ant-select-dropdown-placement-bottomLeft ant-select-dropdown-empty
+// ant-select-dropdown css-dev-only-do-not-override-yp8pcc ant-select-dropdown-placement-bottomLeft"
+// ant-select-item ant-select-item-option ant-select-item-option-active"
