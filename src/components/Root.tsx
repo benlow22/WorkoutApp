5161dbbd0145048958420086c3a27a3eeff32e83
benlow22 <ben2.2low@gmail.com>
2023-05-28 16:22:39 -0700

    remove redundant imports from Root

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,9 +1,5 @@
-import React, { useContext, useEffect } from "react";
 import { Header } from "./header/Header";
-import { useNavigate, Outlet } from "react-router-dom";
-import { AuthContext } from "../contexts/AuthProvider";
-import { LoginPage } from "../pages/login/LoginPage";
-import { WelcomePage } from "../pages/welcomePage/WelcomePage";
+import { Outlet } from "react-router-dom";
 
 export default function Root() {
 	return (
