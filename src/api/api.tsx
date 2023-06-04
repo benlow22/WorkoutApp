fc147c6bb0a40d80152d41aa574bd6324cf50267
benlow22 <ben2.2low@gmail.com>
2023-06-04 15:42:39 -0700

    store API_ENDPOINT as .env var

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,4 +1,4 @@
-const API_ENDPOINT = "https://workout-app-express-backend.vercel.app";
+const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
 import { IWorkoutWithExercises } from "../data";
 import { IExercise, IWorkout } from "../data";
 import { supabase } from "../supabaseClient";
