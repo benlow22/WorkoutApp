92fe3b822c8f82140bcee8385ce6a6af424baa04
benlow22 <ben2.2low@gmail.com>
2023-06-07 15:37:51 -0700

    use REG ex cors options instead of seheadermiddlew

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -52,12 +52,19 @@ export const getAllUsersWorkoutsAPI = async () => {
 	// 	return;
 	// }
 	const response = await fetch(`${API_ENDPOINT}/api/authorized/workouts`, {
-		credentials: "include",
+		method: "GET", // *GET, POST, PUT, DELETE, etc.
+		mode: "cors", // no-cors, *cors, same-origin
+		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+		// credentials: "same-origin", // include, *same-origin, omit
 		headers: {
-			"Access-Control-Allow-Origin":
-				"https://test-workout-app-vercel.vercel.app",
-			"Access-Control-Allow-Credentials": "true",
+			"Content-Type": "application/json",
+			// 'Content-Type': 'application/x-www-form-urlencoded',
 		},
+		redirect: "follow", // manual, *follow, error
+		// referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+		// body: JSON.stringify(data), // body data type must match "Content-Type" header
+
+		credentials: "include",
 	});
 	const data = await response.json(); // returns an array of workouts
 	return data;
