ebc038e9c8a3fc7b3f5df6596c66ad647caedd7c
benlow22 <ben2.2low@gmail.com>
2023-06-07 16:56:29 -0700

    express examples

--- backend-express-app/api/index.js
+++ backend-express-app/api/index.js
@@ -109,8 +109,17 @@ publicRouter.get("/", (req, res) => {
 app.get("api/cats", (req, res) => {
 	res.send(...res.header);
 });
-app.get("/api/", (req, res) => {
-	res.send("Homepage");
+
+app.get("/api", (req, res) => {
+	const path = `/api/item/${v4()}`;
+	res.setHeader("Content-Type", "text/html");
+	res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+	res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get("/api/item/:slug", (req, res) => {
+	const { slug } = req.params;
+	res.end(`Item: ${slug}`);
 });
 
 authorizedRouter.get(
