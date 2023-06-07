85dc885f5e62fb59496419a66667ee0ac919bacc
benlow22 <ben2.2low@gmail.com>
2023-06-07 16:28:56 -0700

    allow headers in preflight

--- backend-express-app/api/index.js
+++ backend-express-app/api/index.js
@@ -85,6 +85,10 @@ app.use((req, res, next) => {
 			"https://test-workout-app-vercel.vercel.app"
 		);
 		res.header("Access-Control-Allow-Credentials", "true");
+		res.header(
+			"Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept"
+		);
 		return res.status(200).json({});
 	}
 	next();
