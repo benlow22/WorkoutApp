897d101f7b617d7013c9632d058fec6b64ce6997
benlow22 <ben2.2low@gmail.com>
2023-06-07 16:03:34 -0700

    remove cors options

--- backend-express-app/api/index.js
+++ backend-express-app/api/index.js
@@ -71,7 +71,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static("public"));
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(cors());
 app.use(cookieParser());
 // app.use(setResHeaders);
