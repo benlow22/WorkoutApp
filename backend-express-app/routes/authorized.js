5b1d78291b4b67870f7585faf37f68caa371c4d1
benlow22 <ben2.2low@gmail.com>
2023-07-10 13:25:09 -0700

    move authorized routes to authorize router

--- backend-express-app/routes/authorized.js
+++ backend-express-app/routes/authorized.js
@@ -1,57 +1,21 @@
 // create another router for getting 'authorized' resources
 const { createClient } = require("@supabase/supabase-js");
+const {
+	setAuthorizedSessionMiddleware,
+} = require("../middleWares.js/middleWares");
+
+// create authorized router = /api/authorized/
 var router = require("express").Router();
 
-const supabase = createClient(
-	process.env.SUPABASE_URL,
-	process.env.SUPABASE_ANON_KEY,
-	{
-		auth: {
-			autoRefreshToken: true,
-			persistSession: false,
-			detectSessionInUrl: true,
-		},
-	}
-); // individual products routes
-const setAuthorizedSessionMiddleware = async (req, res, next) => {
-	/* use req.cookies['cookie-name'] for COOKIES, but not vercel
-		const accessToken = req.cookies["my_access_token"];
-		const refreshToken = req.cookies["my_refresh_token"];
-		const user_id = req.cookies["my_user_is"]; */
-
-	const refreshToken = req.headers["refresh-token"]; // do not just use req.cookies, turn into bearer tokens
-	const accessToken = req.headers["access-token"];
-
-	if (refreshToken && accessToken) {
-		const { data, error } = await supabase.auth.setSession({
-			refresh_token: refreshToken,
-			access_token: accessToken,
-		});
-		if (error) {
-			console.error("error fetching session from supabase: ", error);
-		}
-		if (data) {
-			console.log("session Set");
-		}
-	} else {
-		// make sure you handle this case!
-		console.error("User is not authenticated.");
-		const error = new Error(
-			"User is not authenticated! Access and/or refresh token needed"
-		);
-		console.log(error);
-		return res.status(401).send(error.message);
-	}
-	next();
-};
-
+// add verifying middle ware
 router.use(setAuthorizedSessionMiddleware);
 
+//ROUTES
+// GET workouts = get all users workouts
 router.get("/workouts", async (req, res) => {
 	console.log("Get All Workouts API");
 	// const userId = req.headers["user-id"];
-
-	const { data, error } = await supabase.from("workouts").select("*");
+	const { data, error } = await req.supabase.from("workouts").select("*");
 	if (data) {
 		console.log(data);
 		// if there is data, send it back = 200 status
@@ -59,6 +23,40 @@ router.get("/workouts", async (req, res) => {
 	} else {
 		// if there is no data, send a 404 status with the err
 		res.status(404).send(error);
+	}
+});
+
+// GET /workouts/:workoutUrl = get user's workout data from url
+router.get("/workouts/:workoutUrl", async (req, res) => {
+	const workoutUrl = req.params.workoutUrl;
+	const { data, error } = await req.supabase
+		.from("workouts")
+		.select("*, exercises(*)")
+		.eq("url", workoutUrl)
+		.single(); // get single row as object instead of arr
+	console.log(":workoutUrl DATA", data); // show in terminal
+	if (data) {
+		// if there is data, send it back = 200 status
+		console.log(data);
+		res.send(data);
+	} else {
+		res.status(404).send(error);
+	}
+});
+
+// delete workout using id
+router.delete("/workouts/:workoutId", async (req, res) => {
+	const workoutId = req.params.workoutId;
+	const { data, error } = await req.supabase
+		.from("workouts")
+		.delete()
+		.eq("id", workoutId);
+	console.log("deleted workout: ", data); // show in terminal
+	if (error) {
+		// if there is data, send it back = 200 status
+		res.status(404).send(error);
+	} else {
+		res.status(204);
 	}
 });
 
