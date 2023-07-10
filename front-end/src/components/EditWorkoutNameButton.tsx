7d5fb556519c1393821232a0edc365775dedcc43
benlow22 <ben2.2low@gmail.com>
2023-07-10 12:40:14 -0700

    remove unused imports

--- front-end/src/components/EditWorkoutNameButton.tsx
+++ front-end/src/components/EditWorkoutNameButton.tsx
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Button, Space, Input } from "antd";
-import { workouts, IWorkoutNameUrl } from "../data";
 import { useLocation } from "react-router";
 import { EditTwoTone } from "@ant-design/icons";
 import { updateWorkoutName } from "../api/api";
