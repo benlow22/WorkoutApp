2aa23599e2626b7d85c9e8142d9d9e923bcd4e13
benlow22 <ben2.2low@gmail.com>
2023-07-25 15:02:19 -0700

    install body parser to BE and create public router

--- backend-express-app/routes/index.js
+++ backend-express-app/routes/index.js
@@ -1,5 +1,6 @@
 // routes/index.js
 var router = require("express").Router();
 router.use("/authorized", require("./authorized")); // api/products/
+router.use("/public", require("./public")); // api/products/
 
 module.exports = router;
---
+++ backend-express-app/routes/public.js
@@ -0,0 +1,80 @@
+// create another router for getting 'authorized' resources
+const { createClient } = require("@supabase/supabase-js");
+const supabase = createClient(
+	process.env.SUPABASE_URL,
+	process.env.SUPABASE_ANON_KEY,
+	{
+		auth: {
+			autoRefreshToken: true,
+			persistSession: false,
+			detectSessionInUrl: true,
+		},
+	}
+);
+
+var router = require("express").Router();
+
+// EXERCISES
+router.get("/exercises", async (req, res) => {
+	const { data, error } = await supabase.from("exercise").select("*");
+	console.log("get exercises", data); // show in terminal
+	if (data) {
+		res.send(data);
+	} else {
+		res.status(404).send(error);
+	}
+});
+
+router.get("/cats", (req, res) => {
+	res.send(res.header);
+});
+
+router.get("/", (req, res) => {
+	const path = `/api/item/${v4()}`;
+	res.setHeader("Content-Type", "text/html");
+	res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+	res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+router.get("/api/item/:slug", (req, res) => {
+	const { slug } = req.params;
+	res.end(`Item: ${slug}`);
+});
+
+// // /api/authorized/:exerciseId = get users exercise id including sets and reps
+// router.get("/:exerciseId", async (req, res) => {
+// 	const exerciseId = req.params.exerciseId;
+// 	const { data, error } = await req.supabase
+// 		.from("workouts")
+// 		.delete()
+// 		.eq("id");
+// 	console.log("deleted workout: ", data); // show in terminal
+// 	if (error) {
+// 		// if there is data, send it back = 200 status
+// 		res.status(404).send(error);
+// 	} else {
+// 		res.status(204);
+// 	}
+// });
+
+// // POST /api/authorized/:workoutId/:exerciseId
+// router.post("/:workoutId/:exerciseId", async (req, res) => {
+// 	const workoutId = req.params.workoutId;
+// 	const exerciseId = req.params.exerciseId;
+// 	const userId = req.headers["user-id"];
+// 	console.log(userId, workoutId, exerciseId);
+// 	const { data, error } = await req.supabase
+// 		.from("workouts_exercises")
+// 		.insert([
+// 			{
+// 				workout_id: workoutId,
+// 				exercise_id: exerciseId,
+// 				created_by: userId,
+// 			},
+// 		])
+// 		.select("*");
+// 	console.log("errrror", error);
+// 	console.log("updated workout exercises", data);
+// });
+
+module.exports = router;
