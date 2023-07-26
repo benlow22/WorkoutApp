aa1e72b7e1ea75aed64f8f179b452838461bfbb8
benlow22 <ben2.2low@gmail.com>
2023-07-25 18:13:07 -0700

    add handleAddSet function for +set button

--- front-end/src/components/exercises/AddExerciseData.tsx
+++ front-end/src/components/exercises/AddExerciseData.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Set } from "./sets/set";
+import { Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 
 const testData = {
 	name: "Preacher Curls",
@@ -18,6 +20,13 @@ export const AddExerciseData = ({ exercise }: any) => {
 	);
 	// get exercise data
 	//
+	const handleAddSet = () => {
+		const newSet = weightAndRepsArr[weightAndRepsArr.length - 1];
+		const newWeightAndRepsArr = new Array(...weightAndRepsArr);
+		newWeightAndRepsArr.push(newSet);
+		setWeightAndRepsArr(newWeightAndRepsArr);
+	};
+
 	return (
 		<div className="add-exercise-data-box">
 			<h3 className="add-exercise-data-exercise-name">{exercise.name}</h3>
@@ -29,6 +38,25 @@ export const AddExerciseData = ({ exercise }: any) => {
 					setWeightAndReps={setWeightAndRepsArr}
 				/>
 			))}
+			<div className="confirmation-buttons">
+				<Button
+					type="primary"
+					icon={<PlusOutlined />}
+					onClick={handleAddSet}
+					className="add-set-button"
+				>
+					Add Set
+				</Button>
+				<Button
+					type="primary"
+					icon={<PlusOutlined />}
+					// onClick={handleAddSet}
+					className="add-set-button"
+					style={{ backgroundColor: "Green" }}
+				>
+					Confirm Exercise
+				</Button>
+			</div>
 		</div>
 	);
 };
