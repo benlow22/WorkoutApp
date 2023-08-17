5f0868a01db74f162e4ce0a5ec85adb90a69b6d2
benlow22 <ben2.2low@gmail.com>
2023-08-17 04:27:30 -0700

    get Prev domain from domain location

--- front-end/src/AuthRoute.tsx
+++ front-end/src/AuthRoute.tsx
@@ -4,18 +4,30 @@ import { useContext, useEffect, useState } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { AuthContext } from "./contexts/AuthProvider";
 import { SpiningLoadingIcon } from "./components/loading/LoadingIcon";
+import { domains } from "./utils/utils";
 
 const AuthRoute = () => {
 	const { auth, contextIsLoading } = useContext(AuthContext);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 	const location = useLocation();
+	const [previousDomain, setPreviousDomain] = useState<string>();
 
 	useEffect(() => {
 		if (!contextIsLoading && auth !== undefined) {
-			// console.log("AuthPAge, ", location);
+			console.log("AuthRoute: location of URL, ", location);
+			// extract just DOMAIN
+			const splitPathName = location.pathname.split("/");
+			console.log("split URL ", splitPathName);
+			const subDomain =
+				splitPathName[1] in domains ? splitPathName[1] : "buddySystem";
+			setPreviousDomain(subDomain);
 			setIsLoading(false);
 		}
 	}, [auth]);
+
+	useEffect(() => {
+		console.log("AuthRoute: previousDomain: ", previousDomain);
+	}, [previousDomain]);
 
 	if (!isLoading && auth !== undefined) {
 		return auth ? (
@@ -23,7 +35,10 @@ const AuthRoute = () => {
 		) : (
 			<Navigate
 				to="/login"
-				state={{ initialUrl: location, fromLogin: false }}
+				state={{
+					initialUrl: location.pathname,
+					previousDomain: previousDomain,
+				}}
 			/>
 		);
 	} else {
