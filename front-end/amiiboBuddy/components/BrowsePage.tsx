bc5ea03040e3695d46c22d6ed509dc09be4d0c42
benlow22 <ben2.2low@gmail.com>
2023-08-18 01:00:36 -0700

    add browse, add amiibo homepage routes

--- front-end/amiiboBuddy/components/BrowsePage.tsx
+++ front-end/amiiboBuddy/components/BrowsePage.tsx
@@ -2,11 +2,11 @@ import React, { useContext, useEffect, useState } from "react";
 import { SpiningLoadingIcon } from "../../src/components/loading/LoadingIcon";
 import { amiiboFetchApi } from "../api/api";
 import { supabase } from "../../src/supabaseClient";
-import { AmiiboCard } from "../components/AmiiboCard";
+import { AmiiboCard } from "./AmiiboCard";
 import { TAmiiboCard } from "../types/types";
 import "../styles/amiibos.css";
 
-export const AmiiboBuddyHomePage: React.FC<{}> = () => {
+export const BrowsePage: React.FC<{}> = () => {
 	// const { workouts, setWorkouts, userId, session } = useContext(AuthContext);
 	const [userId, setUserId] = useState("");
 	const [amiibos, setAmiibos] = useState<TAmiiboCard[]>([]);
