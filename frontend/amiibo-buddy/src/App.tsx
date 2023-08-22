db324555534724f3a398573ed607c6a9275cb444
benlow22 <ben2.2low@gmail.com>
2023-08-22 02:47:11 -0700

    add supabase to bs and amiibobrowsw to ab

--- frontend/amiibo-buddy/src/App.tsx
+++ frontend/amiibo-buddy/src/App.tsx
@@ -1,14 +1,43 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	useParams,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from "react-router-dom";
 
 import "./index.css";
+import { AmiiboBuddyDashboard } from "./pages/Dashboard";
 
-const App = () => (
-  <div className="container">
-    <div>Name: amiiboBuddy</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Empty CSS</div>
-  </div>
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<AmiiboBuddyDashboard />}>
+			{/* // 	<Route index element={<WelcomePage />} />
+		// 	<Route path="login" element={<LoginPage />} />
+		// 	<Route element={<AuthRoute />}> */}
+			{/* only authenticated users can use these routes  */}
+			<Route path="amiiboBuddy" element={<AmiiboBuddyDashboard />} />
+			<Route path="amiiboBuddy/browse" element={<BrowsePage />} />
+			<Route
+				path="amiiboBuddy/addAmiibo"
+				element={<AmiiboInventoryForm />}
+			/>
+			<Route path="lorcanaBuddy" element={<LorcanaBuddyDashboard />}>
+				<Route path="browse" element={<BrowsePage />} />
+				<Route path="myCollection" element={<BrowsePage />} />
+				<Route path="browse" element={<BrowsePage />} />
+				{/* </Route> */}
+			</Route>
+		</Route>
+	)
 );
+
+export default function App() {
+	return <RouterProvider router={router} />;
+}
+
 ReactDOM.render(<App />, document.getElementById("app"));
---
+++ frontend/amiibo-buddy/src/utils/types.ts
@@ -0,0 +1,15 @@
+export type TAmiiboCard = {
+	name: string;
+	amiiboSeries: string;
+	character: string;
+	gameSeries: string;
+	head: string;
+	image: string;
+	release_au: string;
+	release_eu: string;
+	release_jp: string;
+	release_na: string;
+	tail: string;
+	type: string;
+	id: string;
+};
