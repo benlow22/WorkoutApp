d077fbdd90fa87a1fac200c8b3f9d4131ceb331b
benlow22 <ben2.2low@gmail.com>
2023-08-26 00:50:54 -0700

    remoce carousel Image

--- front-end/src/apps/amiiboBuddy/src/components/CarouselImage.tsx
+++
@@ -1,6 +0,0 @@
-import React, { useState } from "react";
-import { Carousel } from "antd";
-
-export const CarouselImage = ({ src, alt }: any) => {
-	return <Image src={src} alt={alt} preview={false} />;
-};
