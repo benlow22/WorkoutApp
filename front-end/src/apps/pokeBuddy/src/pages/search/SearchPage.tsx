3597ff8885dcb6801486596a05d556de736f23cd
benlow22 <ben2.2low@gmail.com>
2023-08-29 13:06:39 -0700

    create pokemon serch bar

--- front-end/src/apps/pokeBuddy/src/pages/search/SearchPage.tsx
+++ front-end/src/apps/pokeBuddy/src/pages/search/SearchPage.tsx
@@ -1,24 +1,67 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../../../contexts/AuthProvider";
-
+import { AutoComplete, Input } from "antd";
+import pokemonsJSON from "../../../public/pokemon.json";
+import { DefaultOptionType } from "antd/es/select";
 type TProps = {};
 
 export const SearchPage = ({}: TProps) => {
-	const [amiiboNameSize, setAmiiboNameSize] = useState("");
+	const [pokemons, setPokemons] = useState<any>();
+	const [pokemon, setPokemon] = useState("");
+	const [pokemonsOptions, setPokemonsOptions] = useState<{ value: string }[]>(
+		[]
+	);
+
 	const { userId, session, supabase } = useContext(AuthContext);
 
-	useEffect(() => {}, []);
+	useEffect(() => {
+		if (pokemon) {
+			console.log("chosen mon", pokemon);
+		}
+	}, [pokemon]);
+	useEffect(() => {
+		const pokemonsList = JSON.parse(JSON.stringify(pokemonsJSON));
+		console.log("json", pokemonsList);
+		setPokemons(pokemonsList);
+		const convertPokemonListToOptions = pokemonsList.map(
+			(pokemon: any, index: number) => ({
+				label: <p style={{ color: "black" }}>{pokemon.speciesName}</p>,
+				value: pokemon.speciesName,
+			})
+		);
+		setPokemonsOptions(convertPokemonListToOptions);
+		console.log(convertPokemonListToOptions);
+	}, []);
+
+	const options = [{ value: "?" }];
 
 	return (
-		<div className="amiibo-filter-nav">
-			<h3>Sort By</h3>
-			<p>series</p>
-			<p>Owned</p>
-			<p>Wishlist</p>
-			<p>Return</p>
-			<p>multiples</p>
-			<p>release date</p>
-			<p>alphabetical</p>
+		<div className="poke-search-page">
+			<h3 className="page-heading">Search</h3>
+			{/* <Input style={{ width: "400px" }} /> */}
+			<AutoComplete
+				style={{ width: 200 }}
+				options={pokemonsOptions}
+				placeholder="choose a pokemon"
+				allowClear
+				filterOption={(inputValue, option) =>
+					option!
+						.value!.toUpperCase()
+						.indexOf(inputValue.toUpperCase()) !== -1 &&
+					inputValue.length > 1
+				}
+				onSelect={(value) => {
+					const chosen = pokemons.filter((pokemon: any) => {
+						return pokemon.speciesName === value;
+					});
+					setPokemon(chosen[0]);
+				}}
+			/>
+			{pokemon && (
+				<div>
+					<p>{pokemon.speciesName}</p>
+				</div>
+			)}
 		</div>
 	);
 };
