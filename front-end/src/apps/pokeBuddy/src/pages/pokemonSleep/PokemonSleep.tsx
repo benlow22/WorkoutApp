14a7706cd017fb13b53ffd0f6b8b5f00188f4d09
benlow22 <ben2.2low@gmail.com>
2023-08-30 14:10:47 -0700

    make ingredient clickable and change state

--- front-end/src/apps/pokeBuddy/src/pages/pokemonSleep/PokemonSleep.tsx
+++ front-end/src/apps/pokeBuddy/src/pages/pokemonSleep/PokemonSleep.tsx
@@ -7,9 +7,9 @@ import { Button, Cascader, Input, InputNumber, Select, Space } from "antd";
 import ingredientsJSON from "../../../public/pokemonSleepIngredients.json";
 import { Option } from "antd/es/mentions";
 import recipesJSON from "../../../public/pokemonSleepRecipes.json";
-import { varFromDomainsJSON } from "../../../../../utils/utils";
+import { varFromDomainsJSON, varFromJSON } from "../../../../../utils/utils";
 
-type TIngredients = {
+type TIngredient = {
 	id: number;
 	name: string;
 	description: string;
@@ -23,17 +23,39 @@ export const PokemonSleep = () => {
 	const [potSize, setPotSize] = useState<number>(15);
 	const [allRecipes, setAllRecipes] = useState();
 	const [categories, setCategories] = useState();
-	const [ingredients, setIngredients] = useState<TIngredients[]>([]);
+	const [ingredients, setIngredients] = useState<TIngredient[]>([]);
+	const [unlockedIngredients, setUnlockedIngredients] = useState<string[]>(
+		[]
+	);
 
 	useEffect(() => {
-		const categories = varFromDomainsJSON(recipesJSON, "categories");
+		const categories = varFromJSON(recipesJSON, "categories");
 		setCategories(categories);
 		console.log("categories", categories);
-		const ingredients = varFromDomainsJSON(ingredientsJSON, "ingredients");
+		const ingredients = varFromJSON(ingredientsJSON, "ingredients");
 		console.log("ingredients", ingredients);
 		setIngredients(ingredients);
 	}, []);
 
+	const handleIconClick = (ingredient: TIngredient) => {
+		console.log("icon clicked", ingredient.name);
+		const newIngredientArray = new Array(...unlockedIngredients);
+		if (newIngredientArray.includes(ingredient.name)) {
+			const ingredientToRemoveArrIndex = newIngredientArray.indexOf(
+				ingredient.name
+			);
+			newIngredientArray.splice(ingredientToRemoveArrIndex, 1);
+			console.log("newIngredientArray", newIngredientArray);
+			setUnlockedIngredients(newIngredientArray);
+		} else {
+			newIngredientArray.push(ingredient.name);
+			setUnlockedIngredients(newIngredientArray);
+		}
+	};
+
+	useEffect(() => {
+		console.log("unlockedIngredients ARR", unlockedIngredients);
+	}, [unlockedIngredients]);
 	return (
 		<div className="recipe-page">
 			<div className="page-heading">
@@ -56,13 +78,18 @@ export const PokemonSleep = () => {
 						<Button
 							shape="circle"
 							size="large"
-							className="ingredient-button"
+							className={`ingredient-button ${
+								unlockedIngredients.includes(ingredient.name)
+									? "unlocked-ingredient"
+									: ""
+							}`}
 							icon={
 								<img
 									src={`/pokemonSleepIngredients/${ingredient.imageUrl}`}
 									className="ingredient-button-icon"
 								/>
 							}
+							onClick={() => handleIconClick(ingredient)}
 						/>
 					))}
 			</div>
