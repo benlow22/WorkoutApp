8e9d4e6d87f8b9d4ba4c3885ff5fb4814d161ecc
benlow22 <ben2.2low@gmail.com>
2023-09-15 13:06:59 -0700

    update to deploy

--- backend-express-app/routes/index.js
+++ backend-express-app/routes/index.js
@@ -2,5 +2,5 @@
 var router = require("express").Router();
 router.use("/authorized", require("./authorized")); // api/products/
 router.use("/public", require("./public")); // api/products/
-
+// prepare for domain deploy
 module.exports = router;
