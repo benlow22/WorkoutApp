9d7ea53bc000e0cbd92ead5e1b3fd73dc51385d1
benlow22 <ben2.2low@gmail.com>
2023-09-25 16:13:45 -0700

    remove proxy, change origins

--- backend-express-app/api/index.js
+++ backend-express-app/api/index.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 		);
 		res.setHeader(
 			"Access-Control-Allow-Origin",
-			"https://www.buddy-system.me"
+			"https://www.buddy-system.me, https://buddy-system.vercel.app"
 		);
 		res.setHeader("Access-Control-Allow-Credentials", "true");
 		res.setHeader(
