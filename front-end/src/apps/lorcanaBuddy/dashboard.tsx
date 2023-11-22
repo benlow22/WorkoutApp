aac45462159dd1ac74f3464fa764cfb980a4701b
benlow22 <ben2.2low@gmail.com>
2023-11-21 16:20:54 -0800

    update lorcana dashboard

--- front-end/src/apps/lorcanaBuddy/dashboard.tsx
+++ front-end/src/apps/lorcanaBuddy/dashboard.tsx
@@ -5,7 +5,15 @@ export const LorcanaBuddyDashboard = () => {
 	return (
 		<div className="page-heading">
 			<h2>Lorcana Buddy</h2>
-			<Outlet />
+			{/* <Outlet /> */}
+			<h4>
+				Here you can browse through all of the available lorcana cards
+				and catalog which ones you have, want, or like.
+			</h4>
+			<h4>
+				Once logged in you will be able to save your data for quick
+				reference.
+			</h4>
 		</div>
 	);
 };
