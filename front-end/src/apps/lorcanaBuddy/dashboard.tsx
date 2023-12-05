343e25b0ce253e5e3a373a704fad34db67e68781
benlow22 <ben2.2low@gmail.com>
2023-12-04 16:36:42 -0800

    moved lorcana css

--- front-end/src/apps/lorcanaBuddy/dashboard.tsx
+++ front-end/src/apps/lorcanaBuddy/dashboard.tsx
@@ -1,7 +1,49 @@
 import { Outlet } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
+import lorcanaData from "./src/public/updatedLorcanaCards.json";
 
 export const LorcanaBuddyDashboard = () => {
+	const [allLorcanaCards, setAllLorcanaCards] = useState([]);
+	const [cardCache, setCardCache] = useState({});
+
+	const getLorcanaCards = async () => {
+		const cards = lorcanaData;
+		// make lorcana type
+		// convert lorcana type
+
+		const cache = {};
+		// 	const concatData = cards.map((card) => {
+		// 		const concatName = `${card.} - ${amiibo.amiiboSeries} (${amiibo.type})`;
+		// 		amiiboCache[concatName] = amiibo;
+
+		// 		return {
+		// 			value: concatName,
+		// 			label: (
+		// 				<div
+		// 					style={{
+		// 						display: "flex",
+		// 						alignItems: "center",
+		// 					}}
+		// 				>
+		// 					<img
+		// 						src={amiibo.image}
+		// 						style={{ width: "40px", paddingRight: "10px" }}
+		// 					/>
+		// 					{amiibo.name} - {amiibo.amiiboSeries}
+		// 				</div>
+		// 			),
+		// 			children: amiibo,
+		// 		};
+		// 	});
+		// 	if (concatData) {
+		// 		setOptions(concatData);
+		// 		setCache(amiiboCache);
+		// 		setIsLoading(false);
+		// 	}
+		// } else {
+		// 	console.error("ERROR", error);
+	};
+
 	return (
 		<div className="page-heading">
 			<h2>Lorcana Buddy</h2>
