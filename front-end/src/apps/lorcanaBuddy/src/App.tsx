f7aeadbe4b84e4006baa6bb67a4d707c73a135bb
benlow22 <ben2.2low@gmail.com>
2023-12-08 14:46:41 -0800

    add inventory page to lorcanabuddy app

--- front-end/src/apps/lorcanaBuddy/src/App.tsx
+++ front-end/src/apps/lorcanaBuddy/src/App.tsx
@@ -4,39 +4,16 @@ import { MyCollection } from "./pages/myCollection/MyCollection";
 import { Wishlist } from "./pages/wishlist/Wishlist";
 import { BrowsePage } from "./pages/browse/BrowsePage";
 import { AddItems } from "./pages/addItems/AddItems";
+import { Inventory } from "./pages/inventory/Inventory";
 
 export const LorcanaBuddy = [
-	<Route
-		index
-		element={<LorcanaBuddyDashboard />}
-		key={"LB1"}
-	/>,
-	<Route
-		path="myCollection"
-		element={<MyCollection />}
-		key={"LB2"}
-	/>,
-	<Route
-		path="browse"
-		element={<BrowsePage />}
-		key={"LB3"}
-	/>,
-	<Route
-		path="wishlist"
-		element={<Wishlist />}
-		key={"LB4"}
-	/>,
-	<Route
-		path="addItems"
-		element={<AddItems />}
-		key={"LB4"}
-	/>,
-
-	<Route
-		path="*"
-		element={<Navigate to="" />}
-		key={"LB5"}
-	/>,
+	<Route index element={<LorcanaBuddyDashboard />} key={"LB1"} />,
+	<Route path="myCollection" element={<MyCollection />} key={"LB2"} />,
+	<Route path="browse" element={<BrowsePage />} key={"LB3"} />,
+	<Route path="wishlist" element={<Wishlist />} key={"LB4"} />,
+	<Route path="addItems" element={<AddItems />} key={"LB4"} />,
+	<Route path="inventory" element={<Inventory />} key={"LB6"} />,
+	<Route path="*" element={<Navigate to="" />} key={"LB5"} />,
 ];
 
 // export const WorkoutBuddy = [
