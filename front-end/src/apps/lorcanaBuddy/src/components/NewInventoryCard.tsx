7c656b4daca397971a267fc6b6113dc937262745
benlow22 <ben2.2low@gmail.com>
2024-05-30 21:49:50 -0700

    add locations on their own page

--- front-end/src/apps/lorcanaBuddy/src/components/NewInventoryCard.tsx
+++ front-end/src/apps/lorcanaBuddy/src/components/NewInventoryCard.tsx
@@ -8,12 +8,21 @@ import { Image } from "antd";
 
 export const NewInventoryCard = ({ card }: TProps) => {
 	return (
-		<div>
-			<h1>hi</h1>
-			<h2>
-				{card.Card_Num} {card.Set_Num}
-			</h2>
-			<Image width={100} src={card.Image} fallback={"/public/lorcanaRarity/lorcana-cardback.jpg"} />;
+		<div style={{ margin: "3px", display: "flex", flexWrap: "wrap", width: "100px" }}>
+			<Image width={99} style={{ borderRadius: "5px" }} src={card.Image} fallback={"/public/lorcanaRarity/lorcana-cardback.jpg"} />
+			<div style={{ display: "flex", justifyContent: "space-between", width: "100%", height: "13px" }}>
+				<div>
+					<p style={{ color: "black", fontSize: "10px", verticalAlign: "top", width: "19px" }}>{card.Card_Num}</p>
+				</div>
+				<div style={{ display: "flex" }}>
+					<p style={{ color: "#E7C404", fontSize: "12px" }}>&#x25A2;</p>
+					<p style={{ color: "black", fontSize: "12px", padding: "0px 3px" }}>&#x25A2;&#x25A2;&#x25A2;&#x25A2;</p>
+					<p style={{ color: "#7E7E7E", fontSize: "12px", paddingRight: "3px" }}>&#x25A2;</p>
+
+					{/* <p style={{ fontSize: "10px" }}>foils:</p>
+					<p style={{ fontSize: "10px" }}>non-foils:</p> */}
+				</div>
+			</div>
 		</div>
 	);
 };
--- front-end/src/apps/lorcanaBuddy/src/pages/newInventory/NewInventory.tsx
+++ front-end/src/apps/lorcanaBuddy/src/pages/newInventory/NewInventory.tsx
@@ -38,6 +38,8 @@ export type TNewCard = {
 	Willpower: number;
 };
 
+/// when adding new cards,
+// change wave and uncomment
 export const NewInventory = () => {
 	const { auth, userId, session, supabase, usersLorcanaCards, lorcanaCardImages } = useContext(AuthContext);
 	const [allCards, setAllCards] = useState<TNewCard[] | null>();
@@ -59,19 +61,78 @@ export const NewInventory = () => {
 				"Abilities, Artist,Body_Text, Card_Num, Card_Variants, Classifications, Color, Cost, Franchise, ID, Image, Inkable, Lore, Move_Cost, Name, Rarity, Set_ID, Set_Name, Set_Num, Strength, Type, Unique_ID, Willpower"
 			)
 			.order("Set_Num")
-			.order("Card_Num", { ascending: true });
+			.order("Card_Num");
 		if (data) {
-			const sortedData = data.sort((a, b) => {
-				return a.Set_Num - b.Set_Num || a.Card_Num - b.Card_Num;
+			//PUPPIES
+			// let wave3 = data.filter((card) => card.Set_Num === 3);
+			// const sortedData = wave3.sort((a, b) => {
+			// 	return a.Card_Num - b.Card_Num;
+			// });
+			// const notPuppies = wave3.filter((card) => {
+			// 	if (card.ID !== "3-4a" && card.ID !== "3-4b" && card.ID !== "3-4c" && card.ID !== "3-4d" && card.ID !== "3-4e") {
+			// 		return card;
+			// 	}
+			// });
+			// const puppies = wave3.filter((card) => {
+			// 	if (card.ID === "3-4a" || card.ID === "3-4b" || card.ID === "3-4c" || card.ID === "3-4d" || card.ID === "3-4e") {
+			// 		return card;
+			// 	}
+			// });
+			// const sortedNonPup = notPuppies.sort((a, b) => (Number(a.Card_Num) < Number(b.Card_Num) ? -1 : 1));
+			// // let wave3 = sortedData.filter((card) => card.Set_Num === 3);
+			// sortedNonPup.splice(3, 1, ...puppies);
+			// setAllCards(sortedNonPup);
+
+			//NONPUPPIES
+			// let wave = data.filter((card) => card.Set_Num === 1);
+			// const sortedData = wave.sort((a, b) => {
+			// 	return a.Card_Num - b.Card_Num;
+			// });
+			// setAllCards(sortedData);
+
+			//Green and Red
+			let wave = data.filter((card) => {
+				if (
+					card.Set_Num === 3 &&
+					(card.Card_Num == 202 ||
+						card.Card_Num == 203 ||
+						card.Card_Num == 204 ||
+						card.Card_Num == 168 ||
+						card.Card_Num == 170 ||
+						card.Card_Num == 169 ||
+						card.Card_Num == 134 ||
+						card.Card_Num == 135 ||
+						card.Card_Num == 136 ||
+						card.Card_Num == 100 ||
+						card.Card_Num == 101 ||
+						card.Card_Num == 102 ||
+						card.Card_Num == 66 ||
+						card.Card_Num == 67 ||
+						card.Card_Num == 68 ||
+						card.Card_Num == 32 ||
+						card.Card_Num == 33 ||
+						card.Card_Num == 34)
+				) {
+					return card;
+				}
+			});
+			const sortedData = wave.sort((a, b) => {
+				return a.Card_Num - b.Card_Num;
 			});
 			setAllCards(sortedData);
-			setFirstCard(sortedData[0]);
+
+			console.log(
+				"PUPPIES FOUND : ",
+				sortedData.map((card) => card.Card_Num)
+			);
+			// setAllCards(sortedData.filter((card) => card.Set_Num === 3 && card.Card_Num === "7"));
+			// setFirstCard(sortedData[0]);
 			// const cardImages = data.map((card) => {
 			// 	preloadImage(card.Image);
 			// 	return card.Image;
 			// });
 			// setAllCardImages(cardImages);
-			setTestSmallBatch(sortedData.slice(0, 50));
+			// setTestSmallBatch(sortedData.slice(0, 50));
 			// console.log("get all cards", cardImages);
 		} else {
 			console.error(error);
@@ -82,5 +143,57 @@ export const NewInventory = () => {
 		getAllCards();
 	}, []);
 
-	return <div className="inventory-page">{firstCard && testSmallBatch?.map((card) => <NewInventoryCard card={card} />)}</div>;
+	// Page 1
+	const batch1 = allCards?.slice(0, 9);
+	const batch2 = allCards?.slice(9, 18);
+	const batch3 = allCards?.slice(34, 43);
+	const batch4 = allCards?.slice(43, 52);
+	// // #wave 3 puppies
+
+	// const batch2 = allCards?.slice(9, 18);
+	// const batch3 = allCards?.slice(38, 47);
+	// const batch4 = allCards?.slice(47, 56);
+
+	// //page 2
+	// const batch1 = allCards?.slice(18, 27);
+	// const batch2 = allCards?.slice(27, 34);
+	// const batch3 = allCards?.slice(52, 61);
+	// const batch4 = allCards?.slice(61, 68);
+	// #wave 3 puppies
+	// const batch1 = allCards?.slice(18, 27);
+	// const batch2 = allCards?.slice(27, 35);
+	// const batch3 = allCards?.slice(56, 65);
+	// const batch4 = allCards?.slice(65, 69);
+
+	// wave 3 no locations
+	// const batch1 = allCards?.slice(18, 27);
+	// const batch2 = allCards?.slice(27, 34);
+	// const batch3 = allCards?.slice(52, 61);
+	// const batch4 = allCards?.slice(61, 68);
+
+	return (
+		<div className="inventory-page" style={{ backgroundColor: "white", display: "flex", flexWrap: "wrap", marginLeft: "20px", width: "700px" }}>
+			<div className="3x3" style={{ width: "320px", borderRight: "1px solid black" }}>
+				<div style={{ display: "flex", width: "100%", flexWrap: "wrap" }}>
+					{batch1 && batch1?.map((card) => <NewInventoryCard card={card} key={card.ID} />)}
+				</div>
+			</div>
+			<div className="3x3" style={{ width: "320px", borderRight: "1px solid black" }}>
+				<div style={{ display: "flex", width: "100%", flexWrap: "wrap" }}>
+					{batch2 && batch2?.map((card) => <NewInventoryCard card={card} key={card.ID} />)}
+				</div>
+			</div>
+			<p style={{ width: "100%", borderRight: "none" }}></p>
+			<div className="3x3" style={{ width: "320px", borderRight: "1px solid black" }}>
+				<div style={{ display: "flex", width: "100%", flexWrap: "wrap" }}>
+					{batch3 && batch3?.map((card) => <NewInventoryCard card={card} key={card.ID} />)}
+				</div>
+			</div>
+			<div className="3x3" style={{ width: "320px", borderRight: "1px solid black", marginBottom: "5px" }}>
+				<div style={{ display: "flex", width: "100%", flexWrap: "wrap" }}>
+					{batch4 && batch4?.map((card) => <NewInventoryCard card={card} key={card.ID} />)}
+				</div>
+			</div>
+		</div>
+	);
 };
